aaea0ae2d8e308c5cd585f97c94373a9f552c68f
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2024-07-13 17:30:55 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="text-2xl font-semibold text-center my-4 underline">
+      Personal Finance Management App
+    </main>
+  );
+};
+
+export default App;
